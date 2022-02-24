988b0dbf02bffc5b1810e75689b6bbfeba3b5bf4
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-23 20:21:40 -0800

    installed and configured mongoose connection to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const feedRoutes = require('./routes/feed');
 
@@ -16,4 +19,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect(process.env.DB_CONNECT)
+    .then(result => {
+        app.listen(8080);
+
+    })
+    .catch(err => console.log(err));
