4cb5409a967da2e2cae91ff59a2e49e8f6b28abc
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-24 18:38:44 -0800

    connected user/post models + refactored createPost controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
-const post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -20,8 +20,8 @@ exports.getPosts = (req, res, next) => {
         })
         .then(posts => {
             res.status(200)
-                .json({ 
-                    message: 'Fetched all posts!', 
+                .json({
+                    message: 'Fetched all posts!',
                     posts,
                     totalItems
                 });
@@ -50,19 +50,28 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path.replace('\\', '/');
     console.log(imageUrl);
     const title = req.body.title;
+    let creator;
     const content = req.body.content;
     const post = new Post({
         title,
         content,
         imageUrl,
-        creator: { name: 'Adrian' }
+        creator: req.userId
     });
     post.save()
         .then(result => {
-            console.log(result);
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created successfully!',
-                post: result
+                post,
+                creator: { _id: creator._id, name: creator.name }
             });
         })
         .catch(err => {
