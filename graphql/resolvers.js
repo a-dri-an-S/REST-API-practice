0ca2fdb323a7efd627dad43023537ffcba2176bf
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-25 17:33:03 -0800

    handeling erros with graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -9,11 +9,13 @@ module.exports = {
         if (!validator.isEmail(userInput.email)) {
             errors.push({ message: "Email is invalid!" });
         }
-        if (validator.isEmpty(userInput.password) || validator.isLength(userInput.password, { min: 5 })) {
+        if (validator.isEmpty(userInput.password) || validator.isLength(userInput.password, { max: 5 })) {
             errors.push({ message: 'Password is too short!' })
         }
         if (errors.length > 0) {
             const error = new Error('Invalid input.');
+            error.data = errors;
+            error.code = 422;
             throw error;
         }
         const existingUser = await User.findOne({ email: userInput.email });
